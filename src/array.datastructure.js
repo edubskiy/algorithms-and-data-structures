118857d28fad0c710354df9f7d073d7a163352e4
Evgeniy <edubskiy@openwaygroup.com>
2020-03-12 11:03:01 +0300

    Initial array implementation

---
+++ src/array.datastructure.js
@@ -0,0 +1,14 @@
+// Here we are going to implement array
+
+class ArrayDataStructure {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get() { }
+
+  push() { }
+
+  pop() { }
+}
