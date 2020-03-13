2d5d9d121195069d652b569d618128b4b5c465d9
Evgeniy <edubskiy@openwaygroup.com>
2020-03-13 12:40:46 +0300

    Finished implementing array data structure

--- src/array.datastructure.js
+++ src/array.datastructure.js
@@ -24,4 +24,33 @@ class ArrayDataStructure {
 
     return popedItem;
   }
+
+  deleteAtIndex(index) {
+    const item = this.data[index];
+    this.shiftItemsFromIndex(index);
+
+    this.length--;
+
+    return item;
+  }
+
+  shiftItemsFromIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      // let's check, if we have for example { 0: 'a', 1: 'b', 2: 'c', 3: 'd'}
+      // deleteAtIndex(1) => { 0: 'a', 2: 'c', 3: 'd'}
+      // and calling shiftItemsFromIndex(1) => 'b'
+
+      this.data[i] = this.data[i + 1];
+    }
+
+    // last element is still left at the end so we need to remove it
+    delete this.data[this.length - 1];
+  };
 }
+
+let arr = new ArrayDataStructure();
+
+arr.push('a'); // { 1: 'a' }
+arr.push('b'); // { 1: 'a': 2: 'b' }
+arr.push('c'); // { 1: 'a': 2: 'b', 3: 'c' }
+arr.push('d'); // { 1: 'a': 2: 'b', 3: 'c', 4: 'd' }
