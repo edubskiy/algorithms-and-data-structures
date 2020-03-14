af1fb8d62ccd8dd2d934b32b2c7dbc5229a0b099
Evgeniy <edubskiy@openwaygroup.com>
2020-03-14 18:13:27 +0300

    Added hashtables PROS and CONS

---
+++ src/hash-table.datastructure.js
@@ -0,0 +1,8 @@
+// Pros
+// delete O(1)
+// update O(1)
+// lookup O(1) !can be also O(n)
+// search O(1)
+
+// Cons
+// lookup O(1) or O(n) depends if collision of multiple keys at the same address exist
