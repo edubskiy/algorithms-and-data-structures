986d3420ebd7bb73a9baf01aa15bb2deb8a1a33f
Evgeniy <edubskiy@openwaygroup.com>
2020-03-17 08:52:10 +0300

    Added solution 2 for algorithm of first recurring character

--- src/problems/first-recurring-number.js
+++ src/problems/first-recurring-number.js
@@ -2,6 +2,8 @@
 // Given an array [2,5,1,2,3,5,1,2,4];
 // It should return 2
 
+// ------------------ SOLUTION 1 ------------------ //
+
 function findFirstRecurringCharacter(arr) {
   let occuredCharacters = [];
   for (let i = 0; i < arr.length; i++) {
@@ -14,3 +16,18 @@ function findFirstRecurringCharacter(arr) {
 
 // Test
 findFirstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]);
+
+// ------------------ SOLUTION 2 ------------------ //
+
+function findFirstRecurringCharacter2(arr) {
+  let occuredCharacters = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (occuredCharacters[arr[i]] !== undefined) {
+      return arr[i];
+    }
+    occuredCharacters[arr[i]] = '';
+  }
+}
+
+// Test
+findFirstRecurringCharacter2([2, 5, 1, 2, 3, 5, 1, 2, 4]);
