a87d1c21a98fcefece13a095dbc0a3d5934ebfed
Evgeniy <edubskiy@openwaygroup.com>
2020-03-17 12:47:42 +0300

    Added solution 2 with hashtables as more efficient way, changing time complexity from O(n^2) to O(n)

--- src/problems/first-recurring-number.js
+++ src/problems/first-recurring-number.js
@@ -4,18 +4,21 @@
 
 // ------------------ SOLUTION 1 ------------------ //
 
+// O(n^2)
+// This method has one flaw that is if take as example [2,5,5,2,3,5,1,2,4];
+// normally we want to return 5 as first reccoring character, but with this
+// approach we will return 2, because we are comparing 0 character with rest
+// at the beginning. SOLUTION 2 resolves this
 function findFirstRecurringCharacter(arr) {
   let occuredCharacters = [];
   for (let i = 0; i < arr.length; i++) {
-    if (occuredCharacters.indexOf(arr[i]) !== -1) {
-      return arr[i];
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return arr[i];
+      }
     }
-    occuredCharacters.push(arr[i]);
   }
 }
-
-// Test
-findFirstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]);
 
 // ------------------ SOLUTION 2 ------------------ //
 
