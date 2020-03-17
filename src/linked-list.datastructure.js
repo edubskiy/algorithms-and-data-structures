746f36e0a0f9881f9c3e884824c19e0446ada4df
Evgeniy <edubskiy@openwaygroup.com>
2020-03-17 15:09:30 +0300

    Added prepend method to linked list data structure

--- src/linked-list.datastructure.js
+++ src/linked-list.datastructure.js
@@ -29,10 +29,48 @@ class LinkedList {
       next: null,
     };
 
+    this.data = {};
     this.tail = this.head;
     this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    this.length++;
+
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+
+    return this;
   }
 }
 
 const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
 console.log(myLinkedList);
+/**
+LinkedList {
+  head: { value: 1, next: { value: 10, next: [Object] } },
+  data: {},
+  tail: { value: 16, next: null },
+  length: 4
+}
+ */
