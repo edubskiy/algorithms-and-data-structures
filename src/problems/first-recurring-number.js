ff135ad62f8b3ac7fc37f48cca9f0ba03c8eac52
Evgeniy <edubskiy@openwaygroup.com>
2020-03-17 08:27:40 +0300

    Added algorithm to search for first occuring character in the array

--- src/problems/first-recurring-number.js
+++ src/problems/first-recurring-number.js
@@ -3,5 +3,14 @@
 // It should return 2
 
 function findFirstRecurringCharacter(arr) {
-  for (let i = 0; i < arr.length; i++) {}
+  let occuredCharacters = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (occuredCharacters.indexOf(arr[i]) !== -1) {
+      return arr[i];
+    }
+    occuredCharacters.push(arr[i]);
+  }
 }
+
+// Test
+findFirstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]);
