c196c3665864a01c710cd1a832092790c941a0ce
Evgeniy <edubskiy@openwaygroup.com>
2020-03-20 23:18:34 +0300

    Added MaxChar solution

--- src/exercises/maxchar/index.js
+++ src/exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let hash = {};
+
+  for (character of str) {
+    if (!hash[character]) {
+      hash[character] = 1;
+    } else {
+      hash[character]++;
+    }
+  }
+
+  let maxChar = '';
+  let maxCount = 0;
+
+  Object.keys(hash).forEach(char => {
+    if (hash[char] > maxCount) {
+      maxCount += hash[char];
+      maxChar = char;
+    }
+  });
+
+  return maxChar;
+}
 
 module.exports = maxChar;
