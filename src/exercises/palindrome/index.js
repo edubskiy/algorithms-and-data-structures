118bf7f4af06b7d51a7dc7c956ba19d70eb8c70b
Evgeniy <edubskiy@openwaygroup.com>
2020-03-20 18:17:33 +0300

    Added palindrome solution 2 with every function

--- src/exercises/palindrome/index.js
+++ src/exercises/palindrome/index.js
@@ -8,14 +8,28 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  const originalString = str;
-
-  return (
-    str
-      .split('')
-      .reverse()
-      .join('') === originalString
-  );
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
+
+//-------------------- SOLUTION 1 every -----------------//
+
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     return char === str[str.length - i - 1];
+//   });
+// }
+
+//-------------------- SOLUTION 2 reverse and join -----------------//
+
+// function palindrome(str) {
+//   const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//   return reversed === str;
+// }
