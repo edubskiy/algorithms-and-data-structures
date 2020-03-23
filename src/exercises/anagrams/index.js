7b7dc179024fbf9a45362a18869aba268a955241
Evgeniy <edubskiy@openwaygroup.com>
2020-03-24 00:41:39 +0300

    Implemented anagram comparison

--- src/exercises/anagrams/index.js
+++ src/exercises/anagrams/index.js
@@ -8,6 +8,43 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function convertToLowerCaseCharSequence(str) {
+  return str.toLowerCase().replace(/ /g, '').match(/(\w+)/g).join('');
+}
+
+function buildHashOfStringChars(str) {
+  let hash = {};
+
+  for (const char of str) {
+    if ( ! hash[char]) {
+      hash[char] = 0;
+    } else {
+      hash[char]++;
+    }
+  }
+
+  return hash
+}
+
+function anagrams(stringA, stringB) {
+    stringA = convertToLowerCaseCharSequence(stringA);
+    stringB = convertToLowerCaseCharSequence(stringB);
+    
+    console.log(stringA);
+    console.log(stringB);
+    
+
+    let hashA = buildHashOfStringChars(stringA);
+    let hashB = buildHashOfStringChars(stringB);
+    let compareFrom = stringA.length > stringB.length ? stringA : stringB;
+
+    for (char of compareFrom) {
+      if (hashA[char] !== hashB[char]) {
+        return false;
+      }
+    }
+
+    return true;
+}
 
 module.exports = anagrams;
