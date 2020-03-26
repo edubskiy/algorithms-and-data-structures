7dd71782851975eb8de3a3f7b5024270b9919761
Evgeniy <edubskiy@openwaygroup.com>
2020-03-27 01:13:07 +0300

    Added solution for printing pyramid

--- src/exercises/pyramid/index.js
+++ src/exercises/pyramid/index.js
@@ -14,6 +14,25 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let columnCount = n * 2 - 1;
+  let columnHalf = Math.floor(columnCount / 2);
+
+  for (let row = 0; row < n; row++) {
+    let stairMessage = '';
+    let symbolsCount = row * 2 + 1; // 1..3..5
+    let halfSymbolsCount = (symbolsCount - 1) / 2; // 0..1..2
+    for (let column = 0; column < columnCount; column++) {
+      if (column < columnHalf - halfSymbolsCount || column > columnHalf + halfSymbolsCount) {
+        stairMessage += ' '
+      } else {
+        stairMessage += '#';
+      }
+    }
+    console.log(stairMessage);
+  }
+}
+
+pyramid(3);
 
 module.exports = pyramid;
