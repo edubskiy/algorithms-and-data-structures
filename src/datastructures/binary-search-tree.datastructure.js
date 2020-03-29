a79a15c208ec30be3a950ca74c143d2931e9615e
Evgeniy <edubskiy@openwaygroup.com>
2020-03-29 18:38:13 +0300

    Implemented insert method for binary search tree

---
+++ src/datastructures/binary-search-tree.datastructure.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    
+    if ( ! this.root) {
+      this.root = newNode;
+
+      return this;
+    }
+
+    let node = this.root;
+
+    while (node) {
+      if (node.value > value) {
+        if (node.left) {
+          node = node.left;
+        } else {
+          node.left = newNode;
+          break;
+        }
+      } else {
+        if (node.right) {
+          node = node.right;
+        } else {
+          node.right = newNode;
+          break;
+        }
+      }
+    }
+
+    return this;
+  }
+
+  lookup(value) {
+    //Code here
+  }
+}
+
+//     9
+//  4     20
+//1  6  15  170
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+console.log(tree);
