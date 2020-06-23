9859e3eca658a0cb2dab7c89976d011e64db75cf
Evgeniy <edubskiy@openwaygroup.com>
2020-06-23 21:09:00 +0200

    Solved implementation of Queue using two stacks

--- src/exercises/qfroms/index.js
+++ src/exercises/qfroms/index.js
@@ -14,6 +14,66 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(record) {
+    this.stack1.push(record); // [1,2,3]
+    // if (!this.stack1.peek()) {
+    //   this.stack1.push(record); // [0]
+    //   return;
+    // }
+
+    // this.stack2 = new Stack();
+    // this.stack2.push(record);
+
+    // while (this.stack1.peek()) {
+    //   this.stack2.push(this.stack1.pop()); // [2, 1, 0]
+    // }
+
+    // this.stack1 = this.stack2;
+  }
+
+  remove() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop()); // [3,2,1]
+    }
+
+    const record = this.stack2.pop(); // 1 => [3,2]
+
+    this.stack1 = new Stack();
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop()); // [3,2,1]
+    } // [2,3]
+
+    return record;
+  }
+
+  peek() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop()); // [3,2,1]
+    }
+
+    const record = this.stack2.peek(); // 1 => [3,2]
+
+    this.stack1 = new Stack();
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop()); // [3,2,1]
+    } // [2,3]
+
+    return record;
+  }
+}
+
+const q = new Queue();
+q.add(1);
+q.add(2);
+q.add(3);
+console.log(q);
 
 module.exports = Queue;
