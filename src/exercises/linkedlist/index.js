719b5b40ce99139f8b165b36b23951acbc70d55a
Evgeniy <edubskiy@openwaygroup.com>
2020-06-25 17:44:27 +0200

    Implemented InsertAt solution

--- src/exercises/linkedlist/index.js
+++ src/exercises/linkedlist/index.js
@@ -127,6 +127,22 @@ class LinkedList {
 
     previous.next = previous.next.next;
   }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+    }
+
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast();
+
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }
 }
 
 module.exports = { Node, LinkedList };
