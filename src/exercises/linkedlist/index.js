758c9a266eb7edcc806827fd97e1890c7a370d6b
Evgeniy <edubskiy@openwaygroup.com>
2020-06-25 18:00:05 +0200

    More on code reuse

--- src/exercises/linkedlist/index.js
+++ src/exercises/linkedlist/index.js
@@ -15,10 +15,7 @@ class LinkedList {
   }
 
   insertFirst(data) {
-    const node = new Node(data, this.head);
-    this.head = node;
-
-    return node;
+    this.insertAt(data, 0);
   }
 
   size() {
@@ -131,6 +128,7 @@ class LinkedList {
   insertAt(data, index) {
     if (!this.head) {
       this.head = new Node(data);
+      return;
     }
 
     if (index === 0) {
@@ -139,7 +137,6 @@ class LinkedList {
     }
 
     const previous = this.getAt(index - 1) || this.getLast();
-
     const node = new Node(data, previous.next);
     previous.next = node;
   }
