944b76fcf045392188ad63dee7024a943334b290
Evgeniy <edubskiy@openwaygroup.com>
2020-06-25 14:46:56 +0200

    Implemented getLast node solution

--- src/exercises/linkedlist/index.js
+++ src/exercises/linkedlist/index.js
@@ -36,6 +36,16 @@ class LinkedList {
   getFirst() {
     return this.head;
   }
+
+  getLast() {
+    let node = this.head;
+
+    while (node.next) {
+      node = node.next;
+    }
+
+    return node;
+  }
 }
 
 module.exports = { Node, LinkedList };
