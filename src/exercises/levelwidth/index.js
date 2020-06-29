6958c642b4c3ef7e7c9bccc65a89fe00944d374a
Evgeniy <edubskiy@openwaygroup.com>
2020-06-29 11:28:15 +0200

    Added levelwidth solution for tree using breadth first traversal algorithm

--- src/exercises/levelwidth/index.js
+++ src/exercises/levelwidth/index.js
@@ -11,6 +11,26 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let counters = [0];
+  let queue = [root, 's'];
+
+  while (queue.length > 1) {
+    let next = queue.shift();
+
+    if (next === 's') {
+      queue.push('s');
+      counters.push(0);
+    } else {
+      counters[counters.length - 1]++;
+
+      if (next.children && next.children.length) {
+        queue.push(...next.children);
+      }
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
