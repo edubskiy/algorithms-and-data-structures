511564b4e9f0237522678f6ee6286f31e729b819
Evgeniy <edubskiy@openwaygroup.com>
2020-06-29 17:19:37 +0200

    FIX validation BST, before this fix it only went one direction when validating

--- src/exercises/validate/index.js
+++ src/exercises/validate/index.js
@@ -6,16 +6,20 @@
 // the parent
 
 function validate(node, min = null, max = null) {
-  if ((max !== null && node.data > max) || (min !== null && node.data < min)) {
+  if (max !== null && node.data > max) {
     return false;
   }
 
-  if (node.left) {
-    return validate(node.left, min, node.data);
+  if (min !== null && node.data < min) {
+    return false;
   }
 
-  if (node.right) {
-    return validate(node.right, node.data, max);
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
   }
 
   return true;
