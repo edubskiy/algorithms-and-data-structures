ff18ae27e4e80e0a6d7d1702fce0c8a5decf6352
Evgeniy <edubskiy@openwaygroup.com>
2020-06-29 16:48:49 +0200

    Implemented validation of BST using recursion

--- src/exercises/validate/index.js
+++ src/exercises/validate/index.js
@@ -5,6 +5,20 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if ((max !== null && node.data > max) || (min !== null && node.data < min)) {
+    return false;
+  }
+
+  if (node.left) {
+    return validate(node.left, min, node.data);
+  }
+
+  if (node.right) {
+    return validate(node.right, node.data, max);
+  }
+
+  return true;
+}
 
 module.exports = validate;
