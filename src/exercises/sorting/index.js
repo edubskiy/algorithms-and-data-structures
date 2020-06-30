a56d1b4fb829d0e40ebc5249d633ffcffa109e7c
Evgeniy <edubskiy@openwaygroup.com>
2020-06-30 12:49:41 +0200

    Implemented bubblesort

--- src/exercises/sorting/index.js
+++ src/exercises/sorting/index.js
@@ -2,19 +2,24 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  let tmp;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        tmp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
 
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
