015146a813823bdb1ccfed33f62d477889e87554
Evgeniy <edubskiy@openwaygroup.com>
2020-06-30 15:12:10 +0200

    Implemented selection sorting

--- src/exercises/sorting/index.js
+++ src/exercises/sorting/index.js
@@ -16,7 +16,29 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  let indexOfMin;
+  let tmp;
+
+  for (let i = 0; i < arr.length; i++) {
+    indexOfMin = i; // now prove me wrong
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (i !== indexOfMin) {
+      tmp = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = tmp;
+    }
+  }
+
+  console.log('result array');
+  console.log(arr);
+
+  return arr;
+}
 
 function mergeSort(arr) {}
 
