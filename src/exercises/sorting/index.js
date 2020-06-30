52d2f2abb450bf264c1a2ecad54d4821d2f23815
Evgeniy <edubskiy@openwaygroup.com>
2020-06-30 12:51:23 +0200

    FIX bubblesort algorithm

--- src/exercises/sorting/index.js
+++ src/exercises/sorting/index.js
@@ -4,7 +4,7 @@
 function bubbleSort(arr) {
   let tmp;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         tmp = arr[j + 1];
         arr[j + 1] = arr[j];
