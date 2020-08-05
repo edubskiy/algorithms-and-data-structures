7bf122160af01cb4e40dcfe4ae4892346fdf2ab6
Evgeniy Dubskiy <edubskiy@openwaygroup.com>
2020-08-05 14:23:54 +0200

    Added merge sort algorithm

--- src/exercises/sorting/index.js
+++ src/exercises/sorting/index.js
@@ -37,7 +37,17 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergerSort(right));
+}
 
 function merge(left, right) {
   const results = [];
